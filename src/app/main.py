2bb4c25c7ae384bd6096fb5864aa3c81403b789b
Nicolas_dmb <gyfeix@gmail.com>
2026-04-12 21:13:28 +0200

    feat/ delete test logging

--- src/app/main.py
+++ src/app/main.py
@@ -9,7 +9,6 @@ from .router import router
 
 logging.basicConfig(level=logging.INFO)
 logging.info("Starting the application...")
-logging.error("This is a test error log to verify logging configuration.")
 
 app = FastAPI()
 app.state.limiter = limiter
